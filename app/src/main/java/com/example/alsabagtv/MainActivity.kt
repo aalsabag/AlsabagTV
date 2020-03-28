496613a3b478bae42529a8a85f6c1d777de22a0e
aalsabag <ahmed.alsabag@gmail.com>
2020-03-28 17:22:20 -0500

    Fix images/videos not loasding

--- app/src/main/java/com/example/alsabagtv/MainActivity.kt
+++ app/src/main/java/com/example/alsabagtv/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.alsabagtv
 
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebSettings
 import android.webkit.WebView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var superSafeWebView: WebView
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         superSafeWebView = WebView(applicationContext)
+        superSafeWebView.settings.domStorageEnabled = true;
+        superSafeWebView.settings.setAppCacheEnabled(true);
+        superSafeWebView.settings.loadsImagesAutomatically = true;
+        superSafeWebView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW;
+        superSafeWebView.settings.javaScriptEnabled = true
+
         setContentView(superSafeWebView)
         superSafeWebView.loadUrl("http://www.elahmad.com/tv/mobiletv/")
     }
