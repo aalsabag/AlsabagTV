16871a453e358718359d65eca4953fa255bcab51
aalsabag <ahmed.alsabag@gmail.com>
2020-03-28 23:09:39 -0500

    Removing ads

--- app/src/main/java/com/example/alsabagtv/MainActivity.kt
+++ app/src/main/java/com/example/alsabagtv/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.alsabagtv
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,12 +18,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         superSafeWebView = WebView(applicationContext)
+        superSafeWebView.webViewClient = object : WebViewClient(){
+            override fun onPageFinished(view: WebView?, url: String?) {
+                Log.d("CREATED", "Page finished loading")
+                Log.d("CREATED", url )
+                Thread.sleep(5000)
+                if (url != null) {
+                    if (url.contains("id=")){
+                        Log.d("CREATED", "Removing pop up ads")
+                        superSafeWebView.loadUrl(
+                            "javascript:(function() { " +
+                                    "document.getElementsByTagName('iframe')[0].remove();"+
+                                    "})()");
+                    }
+                }
+                super.onPageFinished(view, url)
+            }
+        };
         superSafeWebView.settings.domStorageEnabled = true;
         superSafeWebView.settings.setAppCacheEnabled(true);
         superSafeWebView.settings.loadsImagesAutomatically = true;
         superSafeWebView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW;
         superSafeWebView.settings.javaScriptEnabled = true
-
         setContentView(superSafeWebView)
         superSafeWebView.loadUrl("http://www.elahmad.com/tv/mobiletv/")
     }
